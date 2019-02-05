452594959649a3df30aa7df6c87aa242fe404aad
ajinkyagprdxn <ajinkyag@prdxn.com>
2019-02-05 13:35:29 +0530

    js: Accordion for menus and top button js functionality completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,49 @@
-/* Author: 
+$(function() {
+	var Accordion = function(el, multiple) {
+		this.el = el || {};
+		this.multiple = multiple || false;
 
-*/
+		// Variables privadas
+		var links = this.el.find('.link');
+		// Evento
+		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+	}
 
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next();
 
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
 
+		if (!e.data.multiple) {
+			$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+		};
+	}	
 
+	var accordion = new Accordion($('#accordion'), false);
+});
 
+//Top Button Logic
+var topbtn=document.getElementById("totop");
+topbtn.addEventListener("click",gotop);
 
+var t1 = 0;
+window.onscroll = scroll1;
 
+function scroll1() {
+  var toTop = document.getElementById('totop');
+  window.scrollY > 200 ? toTop.style.display = 'Block' : toTop.style.display = 'none';
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function gotop() {
+  var y1 = window.scrollY;
+  y1 = y1 - 50;
+  window.scrollTo(0, y1);
+  if (y1 > 0) {
+    t1 = setTimeout("gotop()", 10);
+  } else {
+    clearTimeout(t1);
+  }
+}
