9dfad9c876765b8fb1be0737971d703ca6fd30c4
ajinkyagprdxn <ajinkyag@prdxn.com>
2019-02-05 20:25:16 +0530

    js: hamburger functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
-$(function() {
-	var Accordion = function(el, multiple) {
+//Accordian Logic
+$(function () {
+	var Accordion = function (el, multiple) {
 		this.el = el || {};
 		this.multiple = multiple || false;
 
 		// Variables privadas
 		var links = this.el.find('.link');
 		// Evento
-		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+		links.on('click', { el: this.el, multiple: this.multiple }, this.dropdown)
 	}
 
-	Accordion.prototype.dropdown = function(e) {
+	Accordion.prototype.dropdown = function (e) {
 		var $el = e.data.el;
-			$this = $(this),
+		$this = $(this),
 			$next = $this.next();
 
 		$next.slideToggle();
@@ -20,30 +21,43 @@ $(function() {
 		if (!e.data.multiple) {
 			$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
 		};
-	}	
+	}
 
 	var accordion = new Accordion($('#accordion'), false);
 });
 
 //Top Button Logic
-var topbtn=document.getElementById("totop");
-topbtn.addEventListener("click",gotop);
+var topbtn = document.getElementById("totop");
+topbtn.addEventListener("click", gotop);
 
 var t1 = 0;
 window.onscroll = scroll1;
 
 function scroll1() {
-  var toTop = document.getElementById('totop');
-  window.scrollY > 200 ? toTop.style.display = 'Block' : toTop.style.display = 'none';
+	var toTop = document.getElementById('totop');
+	window.scrollY > 200 ? toTop.style.display = 'Block' : toTop.style.display = 'none';
 }
 
 function gotop() {
-  var y1 = window.scrollY;
-  y1 = y1 - 50;
-  window.scrollTo(0, y1);
-  if (y1 > 0) {
-    t1 = setTimeout("gotop()", 10);
-  } else {
-    clearTimeout(t1);
-  }
+	var y1 = window.scrollY;
+	y1 = y1 - 50;
+	window.scrollTo(0, y1);
+	if (y1 > 0) {
+		t1 = setTimeout("gotop()", 10);
+	} else {
+		clearTimeout(t1);
+	}
 }
+
+//Hamburger Logic
+var ham = document.getElementById("hamburger-btn");
+var hamfull = document.getElementById("hamfull");
+var nav = document.getElementById("nav");
+var body = document.querySelector("body");
+
+ham.addEventListener("click", function () {
+	nav.classList.toggle("block");
+	ham.classList.toggle("active");
+	hamfull.classList.toggle("fullscreen");
+	body.classList.toggle("bodyh");
+});
